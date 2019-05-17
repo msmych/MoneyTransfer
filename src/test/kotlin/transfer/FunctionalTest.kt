89838618d5f8217a2c5d2ffd524f820efab7d55c
Смычков Матвей Александрович <MSmychkov@alfabank.ru>
2019-05-17 16:37:40 +0300

    Deleting account test

--- src/test/kotlin/transfer/FunctionalTest.kt
+++ src/test/kotlin/transfer/FunctionalTest.kt
@@ -5,6 +5,7 @@ import com.google.inject.servlet.GuiceFilter
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.servlet.ServletContextHandler.SESSIONS
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
@@ -16,7 +17,7 @@ import transfer.module.AppServletModule
 import java.util.*
 import javax.servlet.DispatcherType
 
-class FunctionalTest {
+internal class FunctionalTest {
 
     companion object {
 
@@ -28,7 +29,7 @@ class FunctionalTest {
         fun startServer() {
             val injector = createInjector(AppServletModule(), AppPersistenceModule())
             restClient = injector.getInstance(RestClient::class.java)
-            val servletContextHandler = ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+            val servletContextHandler = ServletContextHandler(server, "/", SESSIONS)
             servletContextHandler.addFilter(GuiceFilter::class.java, "/*", EnumSet.allOf(DispatcherType::class.java))
             servletContextHandler.addServlet(DefaultServlet::class.java, "/")
             server.start()
@@ -43,11 +44,15 @@ class FunctionalTest {
 
     @Test
     fun test() {
-        assertNull(restClient.get("account?id=Bill", Account::class.java))
+        assertNull(restClient.get("account?id=Billie", Account::class.java))
         createAccount("Billie")
         assertAccountEquals("Billie", 0, restClient.get("account?id=Billie", Account::class.java))
         createAccount("Alice")
         assertAccountEquals("Alice", 0, restClient.get("account?id=Alice", Account::class.java))
+        deleteAccount("Billie")
+        assertNull(restClient.get("account?id=Billie", Account::class.java))
+        deleteAccount("Alice")
+        assertNull(restClient.get("account?id=Alice", Account::class.java))
     }
 
     private fun createAccount(id: String) {
@@ -60,5 +65,11 @@ class FunctionalTest {
         assertNotNull(account)
         assertEquals(id, account?.id)
         assertEquals(balance, account?.balance)
+    }
+
+    private fun deleteAccount(id: String) {
+        val http = restClient.delete("account?id=$id")
+        http.connect()
+        assertEquals(200, http.responseCode)
     }
 }
